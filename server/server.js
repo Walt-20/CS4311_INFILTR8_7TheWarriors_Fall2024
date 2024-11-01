6381110d8da76dc76185726e6cd3431e90938d97
Walt <walt.c0des20@gmail.com>
2024-11-01 13:52:29 -0600

    expanded functionality of server

--- server/server.js
+++ server/server.js
@@ -8,14 +8,36 @@ import cors from 'cors';
 import csv from 'csv-parser';
 
 const app = express();
-const upload = multer({ dest: 'uploads/' });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const targetDir = path.join(__dirname, 'results');
 const jsonFilePath = path.join(targetDir, 'results.json');
 
-app.use(cors());
+
+const uploadDir = path.join(__dirname, 'uploads')
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true })
+}
+
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true })
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir)
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const newFilename = file.filename + '-' + uniqueSuffix + path.extname(file.originalname)
+        cb(null, newFilename)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.use(cors(), express.json());
 
 app.post('/upload', upload.single('file'), (req, res) => {
     console.log(req.file);
@@ -24,6 +46,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
     }
 
     console.log('uploading file');
+    console.log(req.file.path)
     const filePath = path.join(__dirname, req.file.path);
     const rootDir = path.join(__dirname, '..');
     exec(`python parse.py "${filePath}"`, { cwd: rootDir }, (error, stdout, stderr) => {
@@ -66,9 +89,53 @@ app.post('/upload', upload.single('file'), (req, res) => {
         });
 });
 
+app.post('/start-analysis', (req, res) => {
+    const { disallowedIps, disallowedEntryPoints } = req.body;
+
+    if (!disallowedIps || !disallowedEntryPoints ) {
+        return res.status(400).send('Invalid data')
+    }
+
+    console.log('disallowedIps: ', disallowedIps)
+    console.log('disallowedEntryPoints: ', disallowedEntryPoints)
+})
+
 app.get('/results', (req, res) => {
     res.sendFile(jsonFilePath);
 });
+
+function deleteDirectory(directoryPath) {
+    if (fs.existsSync(directoryPath)) {
+        fs.readdirSync(directoryPath).forEach((file) => {
+            const currentPath = path.join(directoryPath, file);
+            if (fs.lstatSync(currentPath).isDirectory()) {
+                deleteDirectory(currentPath);
+            } else {
+                fs.unlinkSync(currentPath);
+            }
+        });
+        fs.rmdirSync(directoryPath)
+    }
+}
+
+function cleanup() {
+    console.log('Cleaning up...')
+    deleteDirectory(uploadDir)
+    deleteDirectory(targetDir)
+    console.log('Cleanup complete.')
+}
+
+process.on('SIGINT', () => {
+    console.log('Shutting Down....')
+    cleanup()
+    process.exit(0)
+})
+
+process.on('SIGTERM', () => {
+    console.log('Shutting Down....')
+    cleanup()
+    process.exit(0)
+})
 
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
