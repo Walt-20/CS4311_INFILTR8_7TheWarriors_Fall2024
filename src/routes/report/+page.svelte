6df544e143f167b3f7046116a8e046e7f87ce355
aholguin21 <aholguin21@miners.utep.edu>
2024-11-12 14:13:41 -0700

    fixed alignment

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -59,11 +59,7 @@
 		<h1 class="text-4xl font-bold text-gray-800 dark:text-gray-200">Report</h1>
 	</div>
 
-	<!-- Menu Toggle Button -->
-	<button
-		class="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md mb-6"
-		on:click={toggleMenu}>☰ Menu</button
-	>
+
 
 	<!-- Go to Current Project Folder Button -->
 	<button class="bg-gray-600 hover:bg-gray-700 text-white py-2 px-6 rounded-md shadow-md mb-6"
@@ -72,19 +68,19 @@
 
 	<!-- Device List -->
 	<div class="border border-gray-300 bg-gray-300 dark:bg-gray-600 rounded-lg shadow-sm mb-6 p-4">
-		<ul class="space-y-2">
-			<li class="font-bold text-gray-700 flex justify-between bg-gray-100 p-2 rounded-md">
+		<ul class="space-y-2 ">
+			<li class="font-bold text-gray-700 flex justify-between bg-gray-100 p-2 rounded-md overflow-x-auto">
 				<span>IP Addresses</span>
 				<span>Device</span>
 				<span>Vulnerability</span>
-				<span>Status</span>
+				<!-- <span>Status</span> -->
 			</li>
 			{#each ips as ip, index}
-				<li class="flex justify-between p-2 rounded-md hover:bg-gray-50">
+				<li class="flex justify-between p-2 rounded-md hover:bg-gray-50 overflow-x-auto">
 					<span>{ip}</span>
 					<span>{entryPoints[index]}</span>
 					<span>{severity[index]}</span>
-					<span>{pluginName[index]}</span>
+					<!-- <span>{pluginName[index]}</span> -->
 				</li>
 			{/each}
 		</ul>
