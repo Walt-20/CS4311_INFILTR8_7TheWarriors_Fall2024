036ab3e8967fc69f18e2ce309ddfe883797203ac
Walt-20 <walt.c0des20@gmail.com>
2024-12-04 08:46:12 -0500

    Updating script

--- linux-installation.sh
+++ linux-installation.sh
@@ -1,5 +1,63 @@
 #! /bin/bash
 
+REQUIRED_JAVA_VERSION=("21" "17")
+REQUIRED_NODE_VERSION="v20.17.0"
+INSTALL_DIR="/opt/infiltr8"
+CONFIG_FILE="$INSTALL_DIR/config.ini"
+
+check_node_version() {
+	if ! command -v node &> /dev/null; then
+		echo "Node.js is not installed. Please install Node.js $REQUIRED_NODE_VERSION."
+		return 1
+	fi
+
+	INSTALLED_NODE_VERSION=$(node -v)
+	if [ "$INSTALLED_NODE_VERSION" != "$REQUIRED_NODE_VERSION" ]; then
+		echo "Incorrect Node.js version. Found $INSTALLED_NODE_VERSION, required $REQUIRED_NODE_VERSION."
+		return 1
+	fi
+	return 0
+}
+
+check_java_version() {
+	if ! command -v java &> /dev/null; then
+		echo "Java is not installed. Please install Java version 21 or 17."
+		return 1
+	fi
+
+	INSTALLED_JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d. -f1)
+	if [[ ! " ${REQUIRED_JAVA_VERSIONS[@]} " =~ " $INSTALLED_JAVA_VERSION " ]]; then
+        	echo "Java version $INSTALLED_JAVA_VERSION is not supported. Required versions are 21 or 17."
+	        return 1
+	fi
+	return 0
+}
+
+if ! check_node_version; then
+	exit 1
+fi
+
+if ! check_java_version; then
+	exit 1
+fi
+
+if [ ! -f "$CONFIG_FILE" ] || ! grep -q "installed = true" "$CONFIG_FILE"; then
+    echo "Installing project to $INSTALL_DIR"
+
+	if [! -f "$CONFIG_FILE" ]; then
+		echo "[Installation]" > "$CONFIG_FILE"
+		echo "installed = false" >> "$CONFIG_FILE"
+	fi
+
+	#install
+
+	sed -i 's/installed = false/installed = true/' "$CONFIG_FILE"
+	echo "Installation completed."
+
+else
+	echo "Project already installed."
+fi
+	echo "Installing INFILTR8."
 current_directory=$(pwd)
 echo $current_directory
 
